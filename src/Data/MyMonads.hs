a455719dedd66d9de2e64b97bc7b6a42e8d23acf
Ajay Kumar Eeralla <aeeralla@galois.com>
2020-04-16 11:18:43 -0700

    write functor instance

--- src/Data/MyMonads.hs
+++ src/Data/MyMonads.hs
@@ -71,7 +71,7 @@ toList (Cons x xs) = x:toList xs
 
 -- | toIList
 --toIList:: [a] -> IList  a
---toIList [] = Nil 
+--toIList [] = Nil
 
 
 -- | iMap method
@@ -83,4 +83,30 @@ iMap f (Cons x xs) = Cons (f x) (iMap f xs)
 -- | Functor instance for IList
 
 instance Functor (IList n) where
-  fmap = iMap
+  --fmap :: (a -> b) -> IList n a -> IList n b
+  -- | Here fmap exactly like iMap
+  fmap g Nil = Nil
+  fmap g (Cons x xs) = Cons (g x) (fmap g xs)
+
+
+
+-- | MyEither
+data MyEither a b c = MyLeft a | Middle b | MyRight c
+  deriving ( Eq
+           , Ord
+           , Read
+           , Show
+           )
+--class Functor f where
+--fmap :: (a->b) -> f a -> f b
+
+--instance Functor (Either a) where
+--  fmap :: (a -> b) -> Either a a -> Either a b
+--  fmap g (Left x) = Left x
+--  fmap g (Right y) = Right (g y)
+
+instance Functor (MyEither a b) where
+  --fmap :: (a->b) -> MyEither a b a -> MyEither a b b
+  fmap g (MyLeft x) = MyLeft x
+  fmap g (Middle x) = Middle x
+  fmap g (MyRight x) = MyRight (g x)
