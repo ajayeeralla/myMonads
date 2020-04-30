1a8f3ca90d4f04e91c6d56778644b148320d6970
Ajay Kumar Eeralla <aeeralla@galois.com>
2020-04-30 16:57:21 -0700

    add example run for Data.Map types

--- programs/CollectionTypes.hs
+++ programs/CollectionTypes.hs
@@ -2,8 +2,22 @@ module CollectionTypes where
 import Data.Map
 --import Data.HashMap
 --import Data.HashSet
-import Data.Set
-import Data.Sequence
+--import Data.Set
+--import Data.Sequence
 
-main :: IO()
-main = putStrLn "In CollectionTypes"
+lettersDict = fromList [(5,"a"), (3,"b")]
+
+
+returnLetter :: Int -> Maybe String
+returnLetter c = do
+    Data.Map.lookup c lettersDict
+
+
+
+returnViewKey :: Map Int String -> Maybe ((Int, String), Map Int String)
+returnViewKey c = do
+    minViewWithKey lettersDict
+
+main = do
+    putStrLn $ "letter with key value 26: " ++ (show (returnLetter 3))
+    putStrLn $ "letter with key value 25: " ++ (show (returnViewKey lettersDict))
